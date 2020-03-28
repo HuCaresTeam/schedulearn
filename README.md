85c7ba8b6fdfecb7f47b771f4cdf244742cb2179
Ainoras Zukauskas <ainoraz@gmail.com>
2020-03-28 18:09:43 +0200

    Initial setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# schedulearn
+# Schedulearn
+
+Scheduler which allows workers to schedule their learning days and share what they've learned with the rest of the organization.
---
+++ schedulearn-frontend/src/HelloWorld.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import dotnetify from 'dotnetify';
+
+dotnetify.hubServerUrl = 'http://localhost:5000';
+
+interface State {
+  Greetings: string;
+  ServerTime: string;
+}
+
+export default class HelloWorld extends React.Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+    dotnetify.react.connect('HelloWorld', this);
+    this.state = { Greetings: '', ServerTime: '' };
+  }
+
+  render(): React.ReactNode {
+    return (
+      <div>
+        <p>{this.state.Greetings}</p>
+        <p>Server time is: {this.state.ServerTime}</p>
+      </div>
+    );
+  }
+}
