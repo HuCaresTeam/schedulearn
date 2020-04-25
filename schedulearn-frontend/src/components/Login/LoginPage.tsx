2eb1e45a270020cc43bbdc646c2ffb460062b67a
DonatasTamosauskas <donatas.tamosauskas@gmail.com>
2020-04-25 23:23:08 +0300

    Move login functionality to LoginPage.tsx

---
+++ schedulearn-frontend/src/components/Login/LoginPage.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import dotnetify, { dotnetifyVM } from "dotnetify";
+
+dotnetify.hubServerUrl = "http://localhost:5000";
+
+interface State {
+  CurrentUser?: User;
+  VM?: dotnetifyVM;
+  UserName: string;
+}
+
+interface User {
+  Id: number;
+  Name: string;
+  Surname: string;
+  JobTitleId: number;
+}
+
+export default class LoginPage extends React.Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+    this.state = { CurrentUser: { Id: 0, Name: "name", Surname: "surname", JobTitleId: 0 }, UserName: "User name" };
+  }
+
+  componentDidMount(): void {
+    const vm = dotnetify.react.connect("UserBaseVM", this);
+    this.setState({ VM: vm });
+  }
+
+  componentWillUnmount(): void {
+    if (this.state.VM !== undefined)
+      this.state.VM.$destroy();
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  $dispatch = (iValue: any): void => {
+    if (this.state.VM !== undefined)
+      this.state.VM.$dispatch(iValue);
+  };
+
+  handleUsername = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    this.setState({ UserName: event.target.value });
+  }
+
+  render(): React.ReactNode {
+    if (this.state.CurrentUser === undefined)
+      return (<div>Fetching data from database</div>);
+
+    return (
+      <div>
+        <span>Add:</span>
+        <input type="text" value={this.state.UserName} onChange={this.handleUsername} />
+        <button onClick={(): void => this.$dispatch({ SetCurrentUser: { Name: this.state.UserName } })}>
+          Send user
+        </button>
+        <p>Current user name: {this.state.CurrentUser.Name}</p>
+      </div>
+    );
+  }
+}
