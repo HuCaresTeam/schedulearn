8b62503027f54e02b1cd0e5873873dee239b7ec8
DonatasTamosauskas <donatas.tamosauskas@gmail.com>
2020-04-25 23:24:15 +0300

    Implement basic routing

--- schedulearn-frontend/src/index.tsx
+++ schedulearn-frontend/src/index.tsx
@@ -5,15 +5,74 @@ import * as serviceWorker from "./serviceWorker";
 // import HelloWorld from './HelloWorld';
 import NestedListExample from "./components/NestedList/NestedListExample";
 import { LearningDayCalendar } from "./server-components/LearningDayCalendar";
+import LoginPage from "./components/Login/LoginPage";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
+
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <HelloWorld /> */}
-    <NestedListExample />
-    <LearningDayCalendar />
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/list_example">Nested List</Link>
+            </li>
+            <li>
+              <Link to="/calendar">Calendar</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          <Route path="/list_example">
+            <NestedListExample />
+          </Route>
+          <Route path="/calendar">
+            <LearningDayCalendar />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root"),
 );
+
+// function Home() {
+//   return <h2>Home</h2>;
+// }
+
+// function About() {
+//   return <h2>About</h2>;
+// }
+
+// function Users() {
+//   return <h2>Users</h2>;
+// }
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     {/* <HelloWorld /> */}
+//     <NestedListExample />
+//     <LearningDayCalendar />
+//   </React.StrictMode>,
+//   document.getElementById("root"),
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
