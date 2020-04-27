c7b78b6b669931ce19be19c4d5dfb7d86cdd5aff
Ainoras Zukauskas <ainoraz@gmail.com>
2020-04-27 19:31:07 +0300

    Added missing .cs files

--- SchedulearnBackend/SchedulearnBackend/BaseVMs/LearningDayVM.cs
+++ SchedulearnBackend/SchedulearnBackend/BaseVMs/LearningDayVM.cs
@@ -10,14 +10,57 @@ namespace SchedulearnBackend.BaseVMs
 {
     public class LearningDayVM : BaseVM
     {
+        public class FlatLearningDay {
+            public int Id { get; set; }
+            public int TopicId { get; set; }
+            public DateTime DateFrom { get; set; }
+            public DateTime DateTo { get; set; }
+            public string Description { get; set; }
+            public string Title { get; set; }
+
+            public FlatLearningDay() { 
+            }
+
+            public FlatLearningDay(LearningDay learningDay) {
+                Id = learningDay.Id;
+                TopicId = learningDay.TopicId;
+                DateFrom = DateTime.SpecifyKind(learningDay.DateFrom, DateTimeKind.Utc);
+                DateTo = DateTime.SpecifyKind(learningDay.DateTo, DateTimeKind.Utc);
+                Description = learningDay.Description;
+                Title = learningDay.Topic.Name;
+            }
+        }
+
+        public class UserLearningDayRequest { 
+            public int Id { get; set; }
+        }
+
         private readonly SchedulearnContext _schedulearnContext;
         private User CurrentUser { get; set; }
 
-        private IEnumerable<LearningDay> UserLearningDays { get; set; }
+        public IEnumerable<FlatLearningDay> UserLearningDays 
+        { 
+            get 
+            {
+                if (CurrentUser == null)
+                    return new List<FlatLearningDay>();
 
-        private IEnumerable<LearningDay> AllLearningDays
+                return _schedulearnContext.LearningDays
+                    .Where(l => l.UserId == CurrentUser.Id)
+                    .ToList()
+                    .Select(l => new FlatLearningDay(l));
+            } 
+        }
+
+        public IEnumerable<FlatLearningDay> AllLearningDays
         {
-            get { return _schedulearnContext.LearningDays.ToList(); }
+            get 
+            { 
+                return _schedulearnContext.LearningDays
+                    .ToList()
+                    .Select(l => new FlatLearningDay(l))
+                    .ToList(); 
+            }
         }
 
         public LearningDayVM(SchedulearnContext schedulearnContext)
@@ -25,13 +68,35 @@ namespace SchedulearnBackend.BaseVMs
             _schedulearnContext = schedulearnContext;
         }
 
-        public Action<User> GetUserLearningDays => user =>
+        public Action<UserLearningDayRequest> GetUserLearningDays => user =>
         {
-            CurrentUser = _schedulearnContext.Users.Where(u => u.Name == user.Name).First();
-            UserLearningDays = _schedulearnContext.LearningDays.Where(l => l.UserId == user.Id);
+            CurrentUser = _schedulearnContext.Users.Where(u => u.Id == user.Id).FirstOrDefault();
+            if (CurrentUser == null)
+                throw new Exception("User does not exist");
 
             Changed(nameof(CurrentUser));
             Changed(nameof(UserLearningDays));
+            PushUpdates();
+        };
+
+        public Action<FlatLearningDay> CreateLearningDay => learningDay =>
+        {
+            if (CurrentUser == null)
+                throw new Exception("User must be logged in");
+
+            var newLearningDay = new LearningDay()
+            {
+                UserId = CurrentUser.Id,
+                TopicId = learningDay.TopicId,
+                DateFrom = learningDay.DateFrom,
+                DateTo = learningDay.DateTo,
+                Description = learningDay.Description
+            };
+            _schedulearnContext.LearningDays.Add(newLearningDay);
+            _schedulearnContext.SaveChanges();
+
+            Changed(nameof(UserLearningDays));
+            Changed(nameof(AllLearningDays));
             PushUpdates();
         };
     }
