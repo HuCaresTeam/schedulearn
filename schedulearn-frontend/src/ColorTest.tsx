1753cda8ebaa7c387bac3b847d5f30d2551b01cd
Ainoras Zukauskas <ainoraz@gmail.com>
2020-05-11 18:16:52 +0300

    added border to test

--- schedulearn-frontend/src/ColorTest.tsx
+++ schedulearn-frontend/src/ColorTest.tsx
@@ -4,22 +4,23 @@ import { ColorMapper } from "./utils/ColorMapper";
 
 export default class ColorTest extends React.Component {
   render(): React.ReactNode {
-    const colors = [...Array(2000).keys()].map((cId) => ColorMapper.generateColorFromId(cId, "#dddddd"));
-    const pixelGridSize = 35;
+    const colors = [...Array(2000).keys()].map((cId) => ({ id: cId, color: ColorMapper.generateColorFromId(cId, "#dddddd") }));
+    const pixelGridSize = 60;
 
     return (
       <div style={{ display: "grid", gridTemplateColumns: `${pixelGridSize}px `.repeat(Math.floor(1800 / pixelGridSize)) }}>
-        {colors.map((color): React.ReactNode => {
+        {colors.map((colorData): React.ReactNode => {
           return (
             <div
-              key={color}
+              key={colorData.color}
               style={{
-                backgroundColor: color,
+                backgroundColor: colorData.color,
+                border: `1px solid ${ColorMapper.generateColorFromId(colorData.id, "#222222")}`,
                 width: pixelGridSize,
                 height: pixelGridSize,
               }}
             >
-              <div style={{ color: ColorMapper.getFontColorByBackgroud(color) }}>Test</div>
+              <div style={{ color: ColorMapper.getFontColorByBackgroud(colorData.color) }}>Test</div>
             </div>
           );
         })}
